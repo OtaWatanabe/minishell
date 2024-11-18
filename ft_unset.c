aa47d1bd6f9c155ace260f56c4a08e7a61311db1
Ota Watanabe <owatanab@c1r1s1.42tokyo.jp>
2024-11-18 17:35:52 +0900

    unset norm ok

--- ft_unset.c
+++ ft_unset.c
@@ -6,62 +6,62 @@
 /*   By: owatanab <owatanab@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 14:28:06 by otawatanabe       #+#    #+#             */
-/*   Updated: 2024/11/18 17:27:48 by owatanab         ###   ########.fr       */
+/*   Updated: 2024/11/18 17:35:24 by owatanab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_minishell.h"
 
-int    delete_words(char **env_array, char *str, int len)
+int	delete_words(char **env_array, char *str, int len)
 {
-    int    i;
+	int	i;
 
-    i = 0;
-    while (env_array[i] != NULL)
-    {
-        if (strncmp(env_array[i], str, len) == 0)
-        {
-            free(env_array[i]);
-            env_array[i] = strdup("");
-            if (env_array[i] == NULL)
-                error_exit("malloc:");
-            break ;
-        }
-        i++;
-    }
-    return (0);
+	i = 0;
+	while (env_array[i] != NULL)
+	{
+		if (strncmp(env_array[i], str, len) == 0)
+		{
+			free(env_array[i]);
+			env_array[i] = strdup("");
+			if (env_array[i] == NULL)
+				error_exit("malloc:");
+			break ;
+		}
+		i++;
+	}
+	return (0);
 }
 
-void    delete_env_path(char **env_path)
+void	delete_env_path(char **env_path)
 {
-    int    i;
-    int    count;
+	int	i;
+	int	count;
 
-    count = 0;
-    i = 0;
-    while (env_path[count] != NULL)
-        count++;
-    while (i < count)
-    {
-        free(env_path[i]);
-        i++;
-    }
+	count = 0;
+	i = 0;
+	while (env_path[count] != NULL)
+		count++;
+	while (i < count)
+	{
+		free(env_path[i]);
+		i++;
+	}
 }
 
-int    ft_unset(t_shell *shell, char **command)
+int	ft_unset(t_shell *shell, char **command)
 {
-    int    i;
+	int	i;
 
-    i = 1;
-    while (command[i] != NULL)
-    {
-        delete_words(shell->env_array, command[i], strlen(command[i]));
-        if (strncmp(command[i], "PATH", 5) == 0)
-        {
-            delete_env_path(shell->env_path);
-            shell->env_path = NULL;
-        }
-        i++;
-    }
-    return (0);
+	i = 1;
+	while (command[i] != NULL)
+	{
+		delete_words(shell->env_array, command[i], strlen(command[i]));
+		if (strncmp(command[i], "PATH", 5) == 0)
+		{
+			delete_env_path(shell->env_path);
+			shell->env_path = NULL;
+		}
+		i++;
+	}
+	return (0);
 }
